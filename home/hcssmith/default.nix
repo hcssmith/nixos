d23abeced7b261d1860d49733ed3db3bcbb2684b
Hallam Smith <me@hcssmith.com>
2022-04-09 10:21:13 +0100

    Separated out email account handling

--- home/hcssmith/default.nix
+++ home/hcssmith/default.nix
@@ -12,67 +12,8 @@
           st-o
         ];
       };
-      accounts.email.accounts = {
-        hcssmith = {
-          primary = true;
-          address = "me@hcssmith.com";
-          flavor = "fastmail.com";
-          gpg = {
-            key = "DFE3DF82F116584A";
-            signByDefault = true;
-          };
-          imap = {
-            host = "imap.fastmail.com";
-            port = 993;
-            tls = {
-              enable = true;
-              useStartTls = false;
-            };
-          };
-          smtp = {
-            host = "smtp.fastmail.com";
-            port = 465;
-            tls = {
-              enable = true;
-              useStartTls = false;
-            };
-          };
-          notmuch = { enable = true; };
-          mbsync = {
-            enable = true;
-            create = "maildir";
-          };
-          userName = "me@hcssmith.com";
-          realName = "Hallam Smith";
-          passwordCommand =
-            "${pkgs.secret}/bin/secret -p hcssmith.com#email -k 0x6777079D642D66A8";
-          msmtp.enable = true;
-          astroid = { enable = true; };
-          neomutt = {
-            enable = true;
-            extraConfig = ''
-              mailboxes =9front  =Archive  =bank  =Drafts  =Forwarded  =gopher-site  =Inbox  =linode  =mortgage  =Notes  =ovh  =Private  =Sent  =Snoozed  =Spam  =Templates  =Trash  =urbit  =WikiTribune
-              set mail_check_stats
-            '';
-          };
-        };
-      };
       programs = {
         gpg = { enable = true; };
-        astroid = {
-          enable = true;
-          extraConfig = {
-            editor.cmd = "st -w %3 -e nvim -c 'colorscheme darkblue' -- %1";
-          };
-          pollScript = ''
-            set -e
-            mbsync -a
-            notmuch new
-          '';
-        };
-        notmuch = { enable = true; };
-        mbsync = { enable = true; };
-        msmtp = { enable = true; };
         firefox = {
           enable = true;
           extensions = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -94,17 +35,12 @@
         };
       };
       services = {
-        mbsync = {
-          enable = true;
-          frequency = "*:0/1";
-        };
         gpg-agent = {
           enable = true;
           defaultCacheTtl = 10006400;
           pinentryFlavor = "qt";
         };
       };
-    };
-  };
-
+  } // import ../email/hcssmith.com {inherit pkgs;};
+};
 }
