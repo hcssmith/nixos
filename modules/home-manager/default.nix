ed76b8dc66eed6bddce3051927a60ff9f4897b28
Hallam Smith <me@hcssmith.com>
2022-05-29 22:56:02 +0100

    Added vscode

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -61,6 +61,15 @@ let
         {
           gpg.enable = true;
           firefox = mkFirefoxConfig x;
+          vscode = {
+            enable = true;
+            package = pkgs.vscodium;    # You can skip this if you want to use the unfree version
+            extensions = with pkgs.vscode-extensions; [
+                # Some example extensions...
+                ms-dotnettools.csharp
+                dracula-theme.theme-dracula
+            ];
+          };
         }
         (mkIf x.enableEmailConfig
         {
